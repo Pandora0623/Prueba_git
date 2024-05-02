fcf86d8e1a805f02a9a770b75864de82aec5d7da
Stefania <cardonastefania200@gmail.com>
2024-05-02 09:37:03 -0500

    Implementacion de clase Persona por Stefania Delgado Cardona

--- src/Persona.java
+++ src/Persona.java
@@ -3,4 +3,11 @@ public class Persona {
     private Integer Edad;
     private String FechaDeNacimiento;
 
+    public Persona(String Nombre, Integer Edad, String FechaDeNacimiento){
+        this.Nombre = Nombre;
+        this.Edad= Edad;
+        this.FechaDeNacimiento = FechaDeNacimiento;
+    }
+
+
 }
